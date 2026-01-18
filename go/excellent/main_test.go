1186b2cadac7c9df97ec16d6c4f6e6627c66426a
xxx0209 <makelotus0209@gmail.com>
2026-01-18 13:35:22 +0000

    go파일 추가

---
+++ go/excellent/main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestEvenOrOdd(t *testing.T) {
+   result := EvenOrOdd(10)
+   if result != "enen" {
+       t.Errorf("expected: even, actual: %s", result)
+   )	   
+
+}
